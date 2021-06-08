d992d1fdd1a9fb6f84ec3ef914e7027e0cc07330
mano0827 <au.dugout@gmail.com>
2021-06-08 14:47:25 +0900

    js課題3−3

--- js/main.js
+++ js/main.js
@@ -4,23 +4,22 @@
   const tableBody = document.getElementById('todo-body')
   const text = document.getElementById('text');
   const todos = [];
-  
-  
-  
+
+
   const workBtn = document.createElement('button');
   workBtn.textContent = '作業中'
   document.querySelector('button').addEventListener('click', () => {
     const todo = {};
     todo.tableComment = text.value;
     todo.tableStatus = workBtn;
-    
+
     if (todo) {
       todos.push(todo);
       text.value = '';
       showTodos();
     }
   });
-  
+
   const showTodos = () => {
     tableBody.textContent = '';
     todos.forEach((todo, number) => {
@@ -30,7 +29,7 @@
       const comment = document.createElement('td');
       const status = document.createElement('td');
       const action = document.createElement('td');
-      
+
       tableId.textContent = number;
       comment.textContent = todo.tableComment;
       const work = todo.tableStatus
@@ -38,39 +37,36 @@
       tableRecord.appendChild(comment);
       tableRecord.appendChild(status);
       status.appendChild(work);
-        tableRecord.appendChild(action);
+      tableRecord.appendChild(action);
 
-        
-        // work.appendChild(createStatusButton());
-        action.appendChild(createRemoveButton(tableRecord));
-      });
+      // work.appendChild(createStatusButton());
+      action.appendChild(createRemoveButton(tableRecord));
+    });
+  };
+
+  // createStatusButton = () => {
+  workBtn.addEventListener('click', () => {
+    if (workBtn.textContent === '作業中') {
+      workBtn.textContent = '完了'
+    } else {
+      workBtn.textContent = '作業中'
     };
-    
-    // createStatusButton = () => {
-    workBtn.addEventListener('click', () => {
-      if (workBtn.textContent === '作業中') {
-        workBtn.textContent = '完了'
-      } else{
-        workBtn.textContent = '作業中'
-      };
-      return workBtn;
-    })
-    
-  
+    return workBtn;
+  })
   // };
-  
+
   createRemoveButton = (tableRecord) => {
     const number = tableRecord.rowIndex - 1;
     const removeBtn = document.createElement('button');
     removeBtn.textContent = '削除'
-    
+
     removeBtn.addEventListener('click', () => {
       todos.splice(number, 1);
       showTodos();
     })
-    
+
     return removeBtn;
-    
+
   };
 }
 
