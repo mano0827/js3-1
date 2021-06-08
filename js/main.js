c4b507cfdf8994cac873888002d2ade81aa2e314
mano0827 <au.dugout@gmail.com>
2021-06-08 14:44:42 +0900

    js課題3−3

--- js/main.js
+++ js/main.js
@@ -4,19 +4,23 @@
   const tableBody = document.getElementById('todo-body')
   const text = document.getElementById('text');
   const todos = [];
-
+  
+  
+  
+  const workBtn = document.createElement('button');
+  workBtn.textContent = '作業中'
   document.querySelector('button').addEventListener('click', () => {
     const todo = {};
     todo.tableComment = text.value;
-    todo.tableStatus = '作業中'
-
+    todo.tableStatus = workBtn;
+    
     if (todo) {
       todos.push(todo);
       text.value = '';
       showTodos();
     }
   });
-
+  
   const showTodos = () => {
     tableBody.textContent = '';
     todos.forEach((todo, number) => {
@@ -26,43 +30,47 @@
       const comment = document.createElement('td');
       const status = document.createElement('td');
       const action = document.createElement('td');
-
+      
       tableId.textContent = number;
       comment.textContent = todo.tableComment;
+      const work = todo.tableStatus
       tableRecord.appendChild(tableId);
       tableRecord.appendChild(comment);
       tableRecord.appendChild(status);
-      tableRecord.appendChild(action);
+      status.appendChild(work);
+        tableRecord.appendChild(action);
 
-      status.appendChild(createStatusButton());
-      action.appendChild(createRemoveButton(tableRecord));
-    });
-  };
-
-  createStatusButton = () => {
-    const statusBtn = document.createElement('button')
-    statusBtn.textContent = '作業中'
-
-    statusBtn.addEventListener('click', () => {
-      if (statusBtn.textContent === '作業中') {
-        statusBtn.textContent = '完了'
-      } else {
-        statusBtn.textContent = '作業中'
-      }
-    });
-    return statusBtn;
-  };
-
+        
+        // work.appendChild(createStatusButton());
+        action.appendChild(createRemoveButton(tableRecord));
+      });
+    };
+    
+    // createStatusButton = () => {
+    workBtn.addEventListener('click', () => {
+      if (workBtn.textContent === '作業中') {
+        workBtn.textContent = '完了'
+      } else{
+        workBtn.textContent = '作業中'
+      };
+      return workBtn;
+    })
+    
+  
+  // };
+  
   createRemoveButton = (tableRecord) => {
     const number = tableRecord.rowIndex - 1;
     const removeBtn = document.createElement('button');
     removeBtn.textContent = '削除'
-
+    
     removeBtn.addEventListener('click', () => {
       todos.splice(number, 1);
       showTodos();
     })
-
+    
     return removeBtn;
+    
   };
 }
+
